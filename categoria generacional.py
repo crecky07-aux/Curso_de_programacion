64ad1aaf539ca760377735f2a7b2663be1b6cd77
crecky <jesusp54r@gmail.com>
2026-05-05 00:20:13 -0400

    Add files via upload
    ejercicios 1 y 2 python curso

---
+++ categoria generacional.py
@@ -0,0 +1,12 @@
+año = int(input("ingresa tu año de nacimiento: "))
+
+if año >= 1946 and año <= 1964:
+    print("Perteneces a la generación boomer (estas viejo hermano)")  
+elif año >= 1965 and año <= 1980:
+    print("Perteneces a la generación de los que creen que se la saben todas xd")
+elif año >= 1981 and año <= 1996:
+    print("Perteneces a la generación de los señores")
+elif año >= 1997 and año <= 2012:
+    print("Perteneces a la generación de los llorones D:")   
+else:
+    print ("Usted no esta dentro del rango de las generaciones no definidas")
