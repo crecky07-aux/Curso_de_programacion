83c7934459d131f531eb2e6bc4d894bc6f8f605b
crecky <jesusp54r@gmail.com>
2026-05-05 23:36:20 -0400

    Add files via upload

---
+++ multiplo de 5.py
@@ -0,0 +1,7 @@
+
+numero = int(input("Introduce un número entero: "))
+
+if numero % 5 == 0:
+    print(f"El número {numero} es múltiplo de 5.  ")
+else:
+    print(f"El número {numero} NO es múltiplo de 5.")
