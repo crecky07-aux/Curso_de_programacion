1cf946d83aaec3fbedf6701ac3f2f040a7c9be87
crecky <jesusp54r@gmail.com>
2026-05-06 23:22:36 -0400

    Add files via upload

---
+++ descuento mixto tienda (fiados).py
@@ -0,0 +1,19 @@
+# Descuento Mixto en Tienda 🛍️
+
+categoria = input("ingrese la categoría del producto (Electronica o Ropa): ").strip().lower().replace("é", "e")
+cantidad = int(input("ingrese la cantidad comprada: "))
+precio_unitario = float(input("ingrese el precio unitario: "))
+
+subtotal = cantidad * precio_unitario
+
+descuento = 0
+if categoria == "electronica" and cantidad >= 3:
+    descuento = subtotal * 0.10
+elif categoria == "ropa" and cantidad >= 5:
+    descuento = subtotal * 0.15
+
+total = subtotal - descuento
+
+print(f"Subtotal: ${subtotal:.2f}")
+print(f"Descuento: ${descuento:.2f}")
+print(f"Total a pagar: ${total:.2f}")
